3563da9265632a010941f2d2870cba0cf8dbf380
intheloop <admin@tiscacatalin.com>
2025-04-13 10:44:21 +0300

    showing current selected piece

--- src/ui/logics.rs
+++ src/ui/logics.rs
@@ -1,18 +1,18 @@
 use crate::ui::{constants, rendering, GameState};
 use macroquad::color::BLACK;
 use macroquad::input::{is_mouse_button_down, mouse_position, MouseButton};
-use macroquad::prelude::{clear_background, next_frame};
+use macroquad::prelude::{clear_background, draw_rectangle, next_frame, Color};
 
 /// used for updating the game logics
 /// gets called in the game loop
 pub async fn update(state: &mut GameState) {
     if is_mouse_button_down(MouseButton::Left) {
-        let (x, y) = (
+        state.selected_piece = Some((
             (mouse_position().0 / constants::board::SQUARE_SIZE as f32).floor() as usize,
             (mouse_position().1 / constants::board::SQUARE_SIZE as f32).floor() as usize
-        );
-
-        println!("{} {}", x, y);
+        ));
+    } else {
+        state.selected_piece = None;
     }
 }
 
@@ -22,6 +22,15 @@ pub async fn render(state: &GameState) {
     clear_background(BLACK);
 
     rendering::draw_board(&state.textures, &state.board);
+    if let Some(coords) = state.selected_piece {
+        draw_rectangle(
+            ((coords.0 as i32) * constants::board::SQUARE_SIZE) as f32,
+            ((coords.1 as i32) * constants::board::SQUARE_SIZE) as f32,
+            constants::board::SQUARE_SIZE as f32,
+            constants::board::SQUARE_SIZE as f32,
+            Color::from_rgba(255, 0, 0, 255),
+        );
+    }
 
     next_frame().await;
 }
