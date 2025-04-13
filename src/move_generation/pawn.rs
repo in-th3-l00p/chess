ad6c4d875f9b7256bd020a76e763020633472bf1
intheloop <admin@tiscacatalin.com>
2025-04-13 11:54:42 +0300

    some newlines were required ngl

--- src/move_generation/pawn.rs
+++ src/move_generation/pawn.rs
@@ -2,7 +2,13 @@ use crate::board::Board;
 use crate::board::color::Color;
 use crate::move_generation::utils::insert_pos;
 
-pub fn generate(board: &Board, moves: &mut Vec<(usize, usize)>, pos: &(i32, i32), color: &Color, has_moved: &bool) {
+pub fn generate(
+    board: &Board,
+    moves: &mut Vec<(usize, usize)>,
+    pos: &(i32, i32),
+    color: &Color,
+    has_moved: &bool
+) {
     let delta = if let Color::White = color { 1 } else { -1 };
     insert_pos(board, moves, (pos.0 + delta, pos.1 + delta));
     if !has_moved {
