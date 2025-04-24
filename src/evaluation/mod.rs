b62c8e6fb3854d3cd2460454f99243222834d272
intheloop <admin@tiscacatalin.com>
2025-04-24 15:49:09 +0300

    using tables for evaluation

--- src/evaluation/mod.rs
+++ src/evaluation/mod.rs
@@ -1,27 +1,53 @@
 mod constants;
 
-use crate::board::Board;
 use crate::board::color::Color;
+use crate::board::Board;
+use crate::board::piece::{Piece, PieceType};
 
-fn calculate_material(board: &Board) -> (i32, i32) {
-    let mut white_material = 0;
-    let mut black_material = 0;
+fn get_white_table_value(piece: &Piece, (x, y): (i32, i32)) -> i32 {
+    match piece.piece_type {
+        PieceType::Pawn { .. } => constants::tables::PAWN_TABLE[y as usize][x as usize],
+        PieceType::Knight => constants::tables::KNIGHT_TABLE[y as usize][x as usize],
+        PieceType::Bishop => constants::tables::BISHOP_TABLE[y as usize][x as usize],
+        PieceType::Rook { .. } => constants::tables::ROOK_TABLE[y as usize][x as usize],
+        PieceType::Queen => constants::tables::QUEEN_TABLE[y as usize][x as usize],
+        PieceType::King { .. } =>
+            constants::tables::KING_MIDDLE_GAME_TABLE[y as usize][x as usize]
+    }
+}
+
+fn get_black_table_value(piece: &Piece, (x, y): (i32, i32)) -> i32 {
+    match piece.piece_type {
+        PieceType::Pawn { .. } => constants::tables::PAWN_TABLE[7 - y as usize][x as usize],
+        PieceType::Knight => constants::tables::KNIGHT_TABLE[7 - y as usize][x as usize],
+        PieceType::Bishop => constants::tables::BISHOP_TABLE[7 - y as usize][x as usize],
+        PieceType::Rook { .. } => constants::tables::ROOK_TABLE[7 - y as usize][x as usize],
+        PieceType::Queen => constants::tables::QUEEN_TABLE[7 - y as usize][x as usize],
+        PieceType::King { .. } =>
+            constants::tables::KING_MIDDLE_GAME_TABLE[7 - y as usize][x as usize]
+    }
+}
+
+pub fn evaluate(board: &Board) -> i32 {
+    let mut white_score = 0;
+    let mut black_score = 0;
 
     for x in 0..8 {
         for y in 0..8 {
             if let Some(piece) = board.get_piece((x, y)) {
                 match piece.color {
-                    Color::White => white_material += piece.get_worth(),
-                    Color::Black => black_material += piece.get_worth(),
+                    Color::White =>
+                        white_score +=
+                            piece.get_worth() +
+                            get_white_table_value(&piece, (x, y)),
+                    Color::Black =>
+                        black_score +=
+                            piece.get_worth() +
+                            get_black_table_value(&piece, (x, y)),
                 }
             }
         }
     }
 
-    (white_material, black_material)
-}
-
-pub fn evaluate(board: &Board) -> i32 {
-    let (white_material, black_material) = calculate_material(board);
-    white_material - black_material
+    white_score - black_score
 }
